12718c2137d56bc7f171dfaf0c78a1d934e10f4f
Andrew Smerdov <andrew.smerdov@gmail.com>
2017-10-20 15:50:53 +0300

    add request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 http.createServer(function(req, res) {
     console.log('working...');
@@ -9,9 +10,19 @@ http.createServer(function(req, res) {
             res.end(fs.readFileSync('index.html'));
             break;
 
-        case 'request':
-            console.log('request');
-            res.end('hi');
+        case '/request':
+            //console.log('request');
+            res.end("hi");
+            break;
+
+        case '/request1':
+            //console.log('request');
+            res.statusCode = 404;
+            res.end("hi");
+            break;
+
+        default:
+            console.log('url.parse(req.url)', url.parse(req.url));
             break;
     }
 }).listen(3000);
